660d457e5edb891cbe880d534776f6a8844e5cb4
Dylan Whichard <dylan@whichard.com>
2012-07-24 15:58:32 -0400

    Fixed editor pane slightly, implemented tabs and file opening

--- src/net/cubeos/metallic/MetallicGUI.java
+++ src/net/cubeos/metallic/MetallicGUI.java
@@ -1,9 +1,11 @@
 package net.cubeos.metallic;
 
+import java.awt.Graphics;
 import java.awt.Menu;
 import java.awt.MenuBar;
 import java.awt.MenuItem;
 import java.awt.MenuShortcut;
+import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -11,9 +13,10 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.nio.CharBuffer;
+import java.util.HashMap;
 
 import javax.swing.*;
+import javax.swing.text.*;
 
 public class MetallicGUI extends JFrame {
 
@@ -22,13 +25,14 @@ public class MetallicGUI extends JFrame {
 	 */
 	private static final long serialVersionUID = 2239551627744634009L;
 	private JTabbedPane tabs;
+	private HashMap<String,OpenedFile> files;
 	
 	public MetallicGUI()
 	{
 		super("Metallic");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setExtendedState(MAXIMIZED_BOTH);
-		
+		files = new HashMap<String,OpenedFile>();
 		tabs = new JTabbedPane();
 		
 		MenuBar menu = new MenuBar();
@@ -43,26 +47,55 @@ public class MetallicGUI extends JFrame {
 			int returnVal = chooser.showOpenDialog(getContentPane());
 			if (returnVal==JFileChooser.APPROVE_OPTION)
 			{
-				tabs.addTab(chooser.getSelectedFile().getName(), openTab(chooser.getSelectedFile()));
+				JPanel newTab = openTab(chooser.getSelectedFile());
+				if (newTab != null) tabs.addTab(chooser.getSelectedFile().getName(), newTab);
+				else JOptionPane.showMessageDialog(getContentPane(), "Unable to open file.", "Error", JOptionPane.ERROR_MESSAGE);
 			}
 		}});
 		fileMenu.add(openButton);
 		
-		
-		MenuItem saveButton = new MenuItem("Save as...");
-		saveButton.setShortcut(new MenuShortcut(KeyEvent.VK_S,true));
+		MenuItem saveButton = new MenuItem("Save");
+		saveButton.setShortcut(new MenuShortcut(KeyEvent.VK_S));	//TODO add save handler
 		
 		saveButton.addActionListener(new ActionListener() {public void actionPerformed(ActionEvent e) {
 			JFileChooser chooser = new JFileChooser("Save as...");
 			int returnVal = chooser.showSaveDialog(getContentPane());
+			if (returnVal==JFileChooser.APPROVE_OPTION)
+			{
+				
+			}
+		}});
+		
+		MenuItem saveAsButton = new MenuItem("Save as...");
+		saveAsButton.setShortcut(new MenuShortcut(KeyEvent.VK_S,true));
+		
+		saveAsButton.addActionListener(new ActionListener() {public void actionPerformed(ActionEvent e) {
+			JFileChooser chooser = new JFileChooser("Save as...");
+			int returnVal = chooser.showSaveDialog(getContentPane());
 			if (returnVal==JFileChooser.APPROVE_OPTION) System.out.print("User saved as " + chooser.getSelectedFile().getName());
 		}});
-		fileMenu.add(saveButton);
+		fileMenu.add(saveAsButton);
 		
 		menu.add(fileMenu);
 		add(tabs);
 		setMenuBar(menu);	
 		setVisible(true);
+	}
+	
+	public boolean saveTab()
+	{
+		return saveTab(getCurrentTab());
+	}
+	
+	public boolean saveTab(int index)
+	{
+		return false;
+	}
+	
+	public OpenedFile getFileInfo(String name)
+	{
+		if (files.get(name)==null) return null;
+		return files.get(name);
 	}
 	
 	public int getCurrentTab()
@@ -83,13 +116,22 @@ public class MetallicGUI extends JFrame {
 					int next = fr.read();
 					if (next==-1) break;
 					buffer += (char)next;
-				}	//this can't be efficient...
+				}	//this can't be the best way to do this
+				
 				JPanel tabContent = new JPanel();
+				tabContent.setName(f.getAbsolutePath());
+				tabContent.setBounds(getBounds());
 				JEditorPane editor = new JEditorPane();
+				editor.setEditorKit(new NumberedEditorKit());
 				editor.setText(buffer);
+				editor.setBounds(tabContent.getBounds());
+				editor.validate();
 				tabContent.add(editor);
+				tabContent.validate();
 				tabs.add(f.getName(),tabContent);
+				fr.close();
 				return tabContent;
+			
 			} catch (FileNotFoundException e)
 			{
 				return null;
@@ -98,4 +140,84 @@ public class MetallicGUI extends JFrame {
 			}
 		} else return null;
 	}
+}
+
+class NumberedEditorKit extends StyledEditorKit
+{
+    public ViewFactory getViewFactory() {
+        return new NumberedViewFactory();
+    }
+}
+
+class NumberedViewFactory implements ViewFactory
+{
+    public View create(Element elem) {
+        String kind = elem.getName();
+        if (kind != null)
+            if (kind.equals(AbstractDocument.ContentElementName)) {
+                return new LabelView(elem);
+            }
+            else if (kind.equals(AbstractDocument.
+                             ParagraphElementName)) {
+//              return new ParagraphView(elem);
+                return new NumberedParagraphView(elem);
+            }
+            else if (kind.equals(AbstractDocument.
+                     SectionElementName)) {
+                return new BoxView(elem, View.Y_AXIS);
+            }
+            else if (kind.equals(StyleConstants.
+                     ComponentElementName)) {
+                return new ComponentView(elem);
+            }
+            else if (kind.equals(StyleConstants.IconElementName)) {
+                return new IconView(elem);
+            }
+        // default to text display
+        return new LabelView(elem);
+    }
+}
+
+class NumberedParagraphView extends ParagraphView
+{
+    public static short NUMBERS_WIDTH=25;
+
+    public NumberedParagraphView(Element e) {
+        super(e);
+        short top = 0;
+        short left = 0;
+        short bottom = 0;
+        short right = 0;
+        this.setInsets(top, left, bottom, right);
+    }
+
+    protected void setInsets(short top, short left, short bottom,
+                             short right) {super.setInsets
+                             (top,(short)(left+NUMBERS_WIDTH),
+                             bottom,right);
+    }
+
+    public void paintChild(Graphics g, Rectangle r, int n) {
+        super.paintChild(g, r, n);
+        int previousLineCount = getPreviousLineCount();
+        int numberX = r.x - getLeftInset();
+        int numberY = r.y + r.height - 5;
+        g.drawString(Integer.toString(previousLineCount + n + 1),
+                                      numberX, numberY);
+    }
+
+    public int getPreviousLineCount() {
+        int lineCount = 0;
+        View parent = this.getParent();
+        int count = parent.getViewCount();
+        for (int i = 0; i < count; i++) {
+            if (parent.getView(i) == this) {
+                break;
+            }
+            else {
+                lineCount += parent.getView(i).getViewCount();
+            }
+        }
+        return lineCount;
+    }
 }
