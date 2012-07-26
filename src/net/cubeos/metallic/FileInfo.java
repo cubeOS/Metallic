3132857ba79b2301bf971f99bb66adfb94ab83f9
Dylan Whichard <dylan@whichard.com>
2012-07-26 00:01:00 -0400

    Forgot to add a class, silly me

---
+++ src/net/cubeos/metallic/FileInfo.java
@@ -0,0 +1,80 @@
+package net.cubeos.metallic;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.WriteAbortedException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class FileInfo
+{
+	private File file;
+	private long openedOn;
+	private long lastSaved;
+	private boolean isModified;
+	
+	public FileInfo(File f)
+	{
+		file = f;
+		openedOn = System.currentTimeMillis();
+		lastSaved = openedOn; 	
+	}
+	
+	public boolean wasModifiedExternally()
+	{
+			//call getAbsoluteFile so that lastModified information is updated
+		return file.getAbsoluteFile().lastModified() > lastSaved;
+	}
+	
+	public void setModified(boolean modified)
+	{
+		isModified = modified;
+	}
+	
+	public String getContents() throws IOException, SecurityException
+	{
+		//List<String> lines = Files.readAllLines(Files., null)
+		String buffer = "";
+		FileReader fr = new FileReader(file);
+		while (true)
+		{
+			int next = fr.read();
+			if (next==-1) break;
+			buffer += (char)next;
+		}	//this can't be the best way to do this...
+		fr.close();
+		return buffer;
+	}
+	
+	public void save(String contents) throws IOException, SecurityException, FileModifiedException
+	{
+		save(contents,false);
+	}
+	
+	public void save(String contents, boolean override) throws IOException, SecurityException, FileModifiedException
+	{
+		if (wasModifiedExternally() && !override) throw new FileModifiedException("File was modified by another program");
+		FileWriter fw = new FileWriter(file);
+		fw.write(contents);
+		fw.close();
+		lastSaved = file.getAbsoluteFile().lastModified();
+	}
+	
+	public class FileModifiedException extends Exception
+	{
+		public FileModifiedException(String m)
+		{
+			super(m);
+		}
+	}
+	
+	public class FileExistsException extends Exception
+	{
+		public FileExistsException(String m)
+		{
+			super(m);
+		}
+	}
+}
